a3e6f0828e22a9f3dc79e2d95faf3314efc854bc
Onur Yağız <95749456+onuryaqiz@users.noreply.github.com>
2022-08-17 18:13:19 +0300

    Add project files.

---
+++ BulkyBookWeb/Program.cs
@@ -0,0 +1,40 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args); // 
+
+// Add services to the container.
+builder.Services.AddControllersWithViews(); // Register our DB we have to create builder . Razor Pages'te burası değişecek.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+
+
+    builder.Configuration.GetConnectionString("DefaultConnection") // DB bağlantısı, appsettings.json'daki SQL server yoludur.
+
+
+
+    ));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // middleware
+
+app.UseRouting(); // middleware
+
+//app.UseAuthentication(); Authoraztion'dan önce gelir. 
+app.UseAuthorization(); // middleware 
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
